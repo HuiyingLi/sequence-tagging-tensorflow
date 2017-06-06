e9ba8ce0d4049b7ec92f9bf9b392b2adae9a926f
HuiyingLi <willwin.lee@gmail.com>
2017-06-06 15:03:13 +0800

    added tf FLAGES for hyperparameters

--- train.py
+++ train.py
@@ -5,6 +5,31 @@ import numpy as np
 import pdb
 import subprocess
 import time
+tf.app.flags.DEFINE_string('pretrain_path', "embeddings/glove.6B.100d.txt", "")
+tf.app.flags.DEFINE_string('train_path', "eng.train", "")
+tf.app.flags.DEFINE_string('validate_path', "eng.dev", "")
+tf.app.flags.DEFINE_string('test_path', "eng.test", "")
+tf.app.flags.DEFINE_string('eval_path', "/home/huiying/workspace/ner-project/tmp/", "")
+tf.app.flags.DEFINE_string('eval_script_path', "/home/huiying/workspace/ner-project/tmp/conlleval", "")
+tf.app.flags.DEFINE_string('checkpoint_path', "/home/huiying/workspace/ner-project/tmp/chkpnt", "")
+
+
+tf.app.flags.DEFINE_integer('batch_size', 10, 'number of sentences in a batch')
+tf.app.flags.DEFINE_integer('num_steps', 30, 'number of steps unrolled in RNN')
+tf.app.flags.DEFINE_integer('max_word_len', 35, 'maximum number of characters contained in a word')
+tf.app.flags.DEFINE_integer('lstm_state_size', 200, 'number of lstm cells')
+tf.app.flags.DEFINE_integer('nfilter', 30, 'number of filters in characterlevel cnn')
+tf.app.flags.DEFINE_integer('filter_size', 3, 'the windows size each filter scans')
+tf.app.flags.DEFINE_integer('char_emb_size', 30, 'the character embedding dimension')
+tf.app.flags.DEFINE_float('dropout', 0, 'dropout probability')
+
+tf.app.flags.DEFINE_integer('total_epoch', 200, 'total number of epochs to run')
+tf.app.flags.DEFINE_float('max_grad_norm', 5.0, 'gradient clipping')
+tf.app.flags.DEFINE_float('learning_rate', 0.015, 'initial learning rate')
+tf.app.flags.DEFINE_float('decay_rate', 0.05, 'the decay rate of learning rate')
+tf.app.flags.DEFINE_boolean('crf', True, 'if crf layer is used')
+
+FLAGS = tf.app.flags.FLAGS
 
 def run_epoch(sess, traindata, train_model, lstm_state_fw, lstm_state_bw, batch_size, num_steps):
     avg_loss = 0
@@ -135,6 +160,7 @@ def evaluate(sess, validate_data, validate_model, batch_size, num_steps, tmpdir)
 
 
 def main():
+    '''
     config = reader.read_parameters("config.txt")
     max_word_len = config['max_word_len']
     total_epoch = config['total_epoch']
@@ -144,37 +170,38 @@ def main():
     learning_rate = config['learning_rate']
     decay_rate = config['decay_rate']
     max_grad_norm = config['max_grad_norm']
+    '''
     pretrain_word2id, pretrain_id2word, pretrain_emb = reader.load_pretrain(
-        config['pretrain_path'],
-        [config['train_path'], config['validate_path'], config['test_path']])
-    vocabs = reader.build_vocab(config['train_path'])
-    traindata = reader.DataSet(config['train_path'], max_word_len,
+        FLAGS.pretrain_path,
+        [FLAGS.train_path, FLAGS.validate_path, FLAGS.test_path])
+    vocabs = reader.build_vocab(FLAGS.train_path)
+    traindata = reader.DataSet(FLAGS.train_path, FLAGS.max_word_len,
                                pretrain_word2id, pretrain_id2word, pretrain_emb, vocabs)
     traindata.load_data()
-    validate = reader.DataSet(config['validate_path'], max_word_len,
+    validate = reader.DataSet(FLAGS.validate_path, FLAGS.max_word_len,
                               pretrain_word2id, pretrain_id2word, pretrain_emb, vocabs)
     validate.load_data()
-    test = reader.DataSet(config['test_path'], max_word_len,
+    test = reader.DataSet(FLAGS.test_path, FLAGS.max_word_len,
                           pretrain_word2id, pretrain_id2word, pretrain_emb, vocabs)
     test.load_data()
-    seq_lens = num_steps * np.ones(batch_size)
+    seq_lens = FLAGS.num_steps * np.ones(FLAGS.batch_size)
 
     with tf.Graph().as_default(), tf.Session() as sess:
         with tf.variable_scope("Model"):
             train_model = model.inference_graph(
                 char_vocab_size=len(traindata.char2id),
                 pretrain_embedding=traindata.pretrain_emb,
-                max_word_len=max_word_len,
+                max_word_len=FLAGS.max_word_len,
                 ntags=len(traindata.tag2id),
-                batch_size=batch_size,
-                num_steps=num_steps,
-                char_emb_size=config['char_emb_size'],
-                lstm_state_size=config['lstm_state_size'],
-                dropout=config['dropout'],
-                filter_sizes=[config['filter_size']],
-                nfilters=[config['nfilter']])
-            train_model.update(model.loss_graph(train_model.logits, batch_size, num_steps, crf, seq_lens))
-            train_model.update(model.training_graph(train_model.loss * num_steps, learning_rate, max_grad_norm))
+                batch_size=FLAGS.batch_size,
+                num_steps=FLAGS.num_steps,
+                char_emb_size=FLAGS.char_emb_size,
+                lstm_state_size=FLAGS.lstm_state_size,
+                dropout=FLAGS.dropout,
+                filter_sizes=[FLAGS.filter_size],
+                nfilters=[FLAGS.nfilter])
+            train_model.update(model.loss_graph(train_model.logits, FLAGS.batch_size, FLAGS.num_steps, FLAGS.crf, seq_lens))
+            train_model.update(model.training_graph(train_model.loss * FLAGS.num_steps, FLAGS.learning_rate, FLAGS.max_grad_norm))
             #train_model.update(model.training_graph(train_model.loss))
         saver = tf.train.Saver()
 
@@ -183,32 +210,32 @@ def main():
             validate_model=model.inference_graph(
                 char_vocab_size=len(validate.char2id),
                 pretrain_embedding=validate.pretrain_emb,
-                max_word_len=max_word_len,
+                max_word_len=FLAGS.max_word_len,
                 ntags=len(validate.tag2id),
-                batch_size=batch_size,
-                num_steps=num_steps,
-                char_emb_size=config['char_emb_size'],
-                lstm_state_size=config['lstm_state_size'],
-                dropout=config['dropout'],
-                filter_sizes=[config['filter_size']],
-                nfilters=[config['nfilter']])
-            validate_model.update(model.loss_graph(validate_model.logits, batch_size, num_steps, crf, seq_lens))
+                batch_size=FLAGS.batch_size,
+                num_steps=FLAGS.num_steps,
+                char_emb_size=FLAGS.char_emb_size,
+                lstm_state_size=FLAGS.lstm_state_size,
+                dropout=FLAGS.dropout,
+                filter_sizes=[FLAGS.filter_size],
+                nfilters=[FLAGS.nfilter])
+            validate_model.update(model.loss_graph(validate_model.logits, FLAGS.batch_size, FLAGS.num_steps, FLAGS.crf, seq_lens))
             validate_model.update(model.adict(name="validation"))
         '''Test model'''
         with tf.variable_scope("Model", reuse=True):
             test_model=model.inference_graph(
                 char_vocab_size=len(test.char2id),
                 pretrain_embedding=test.pretrain_emb,
-                max_word_len=max_word_len,
+                max_word_len=FLAGS.max_word_len,
                 ntags=len(test.tag2id),
-                batch_size=batch_size,
-                num_steps=num_steps,
-                char_emb_size=config['char_emb_size'],
-                lstm_state_size=config['lstm_state_size'],
-                dropout=config['dropout'],
-                filter_sizes=[config['filter_size']],
-                nfilters=[config['nfilter']])
-            test_model.update(model.loss_graph(test_model.logits, batch_size, num_steps, crf, seq_lens))
+                batch_size=FLAGS.batch_size,
+                num_steps=FLAGS.num_steps,
+                char_emb_size=FLAGS.char_emb_size,
+                lstm_state_size=FLAGS.lstm_state_size,
+                dropout=FLAGS.dropout,
+                filter_sizes=[FLAGS.filter_size],
+                nfilters=[FLAGS.nfilter])
+            test_model.update(model.loss_graph(test_model.logits, FLAGS.batch_size, FLAGS.num_steps, FLAGS.crf, seq_lens))
             test_model.update(model.adict(name="test"))
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
@@ -218,27 +245,27 @@ def main():
 
 
         current_best_Fscore = 0.0
-        for epoch in range(total_epoch):
+        for epoch in range(FLAGS.total_epoch):
             print "epoch", epoch
             start_time = time.time()
-            loss = run_epoch(sess, traindata, train_model, lstm_state_fw, lstm_state_bw, batch_size, num_steps)
+            loss = run_epoch(sess, traindata, train_model, lstm_state_fw, lstm_state_bw, FLAGS.batch_size, FLAGS.num_steps)
 
-            if crf:
-                Fscore = crf_eval(sess, validate, validate_model, batch_size, num_steps, config['eval_path'], config['eval_script_path'])
+            if FLAGS.crf:
+                Fscore = crf_eval(sess, validate, validate_model, FLAGS.batch_size, FLAGS.num_steps, FLAGS.eval_path, FLAGS.eval_script_path)
             else:
-                Fscore = evaluate(sess, validate, validate_model, batch_size, num_steps, config['eval_path'])
-            if Fscore>current_best_Fscore:
+                Fscore = evaluate(sess, validate, validate_model, FLAGS.batch_size, FLAGS.num_steps, FLAGS.eval_path)
+            if Fscore > current_best_Fscore:
                 current_best_Fscore = Fscore
                 print "**Results on test set with current best F:", current_best_Fscore
-                crf_eval(sess, test, test_model, batch_size, num_steps, config['eval_path'],
-                         config['eval_script_path'])
-                saver.save(sess, config['checkpoint_path'])
+                crf_eval(sess, test, test_model, FLAGS.batch_size, FLAGS.num_steps, FLAGS.eval_path,
+                         FLAGS.eval_script_path)
+                saver.save(sess, FLAGS.checkpoint_path)
                 print "Model saved!"
 
-            new_learning_rate = learning_rate / (1 + decay_rate * (epoch + 1))
+            new_learning_rate = FLAGS.learning_rate / (1 + FLAGS.decay_rate * (epoch + 1))
             sess.run(train_model.learning_rate.assign(new_learning_rate))
             end_time = time.time()
-            print "Epoch training time:", end_time-start_time
+            print "Epoch training time:", end_time - start_time
 
-if __name__== '__main__':
+if __name__ == '__main__':
    main()
